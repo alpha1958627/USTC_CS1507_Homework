3ddd12707a23010af968db568e835a8981494637
alpha1958627 <1967502901@qq.com>
2023-05-07 13:07:54 +0800

    get news catalo

--- crawler.py
+++ crawler.py
@@ -12,26 +12,12 @@ href_list = [f'http://stuhome.ustc.edu.cn/2314/list{i}.htm' for i in range(1, 10
 
 #get the title and href
 
-def Getnewsoutline(i):
-        if i == 0:
-            resp = requests.get('http://stuhome.ustc.edu.cn/2314/list.htm', headers=headers)
-            resp.encoding = 'utf-8'
-            with open(f'./catalog/0.html', 'w+', encoding= 'utf-8') as f:
-                f.write(resp.text)
-        else:
-            resp = requests.get(href_list[i], headers=headers)
-            resp.encoding = 'utf-8'
-            with open(f'./catalog/{i}.html', 'w+', encoding= 'utf-8') as f:
-                f.write(resp.text)
+def Getnewsoutline(i):  
+    resp = requests.get(href_list[i], headers=headers)
+    resp.encoding = 'utf-8'
+    with open(f'./catalog/{i}.html', 'w+', encoding= 'utf-8') as f:
+        f.write(resp.text)
 
-def Getnews(i):
-     pass
-#get 
-
-# resp = requests.get(f'http://stuhome.ustc.edu.cn/2314/list100.htm', headers=headers)
-# resp.encoding = 'utf-8'
-# with open('100.html', 'w+', encoding= 'utf-8') as f:
-#         f.write(resp.text)
 if __name__ == '__main__': 
     for i in range (0,100):
         Getnewsoutline(i)
